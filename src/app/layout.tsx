2523f1fdfb75f0f6b2e8bc3e1e62736541cd4908
JayeshKGP <jayeshdeshmukh@kgpian.iitkgp.ac.in>
2024-11-05 13:24:45 +0530

    auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
+import { ReduxProvider } from './store/provider';
+import { useDispatch } from 'react-redux';
+import { loadUser } from './store/authSlice';
+import AuthInitializer from './components/AuthInit';
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -28,7 +31,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ReduxProvider><AuthInitializer />
+        {children}</ReduxProvider>
       </body>
     </html>
   );
